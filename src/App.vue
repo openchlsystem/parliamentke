f6f567a387cc423b57ef9f2e57ae4e92fffc9481
Nelson Adagi <thinkiqbs@gmail.com>
2023-10-11 13:02:58 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layout">
+    <header class="header">Header 1</header>
+    <header class="header">
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+    <div class="body">
+      <div class="column">Column 1</div>
+      <div class="column"><router-view/></div>
+      <div class="column">Column 3</div>
+      <div>Anything</div>
+    </div>
+    <footer class="footer">Footer 1</footer>
+    <footer class="footer">Footer 2</footer>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +27,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
